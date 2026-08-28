146fcbdd04cb13e6157daac9a5f8a744f74cbaca
Viktor Rådman <viktor.radman@biometria.se>
2026-08-28 22:38:05 +0200

    Add migration for LookingForGameNote column

---
+++ src/Playr.Infrastructure/Migrations/20260828203521_AddLookingForGameNote.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Playr.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLookingForGameNote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LookingForGameNote",
+                table: "UserProfiles",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LookingForGameNote",
+                table: "UserProfiles");
+        }
+    }
+}
